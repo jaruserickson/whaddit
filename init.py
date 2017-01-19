f8342c0cb3666d29cbbccb7f6ced6c0e76bbb4c8
Jacob <jacob.erickson@mail.utoronto.ca>
2017-01-19 14:15:01 -0500

    finished average calculations + clarifai integration)

--- init.py
+++ init.py
@@ -8,7 +8,9 @@ CLARIFAI_APP_ID = "YOUR_ID"
 CLARIFAI_APP_SECRET = "YOUR_SECRET" #hidden!
 
 def subExists(sub):
-	#check the json
+	#(str) -> bool
+	#checks if a subreddit exists, based on its json
+	print("Validating subreddit...")
 	resp = requests.get("http://reddit.com/r/" + sub + ".json", headers={'User-agent' : 'recommeddit:python3:v0.02 (by /u/levelprime)'})
 	data = json.loads(resp.text)
 	#data children will be empty on an invalid sub
@@ -16,6 +18,23 @@ def subExists(sub):
 		return False
 	else:
 		return True
+
+def mostCommon(preds):
+	#(list of lists of str,double) -> dict(str, double)
+	#catered function to find most commonly found values in predicitons 2d list
+	print("Getting most common trends...")
+	common = {}
+	for prediction in predictions:
+		if prediction[0] not in list(common.keys()):
+			common[prediction[0]] = [prediction[1]]
+		else:
+			common[prediction[0]].append(prediction[1])
+	#now calculate the average for prediction values and set the key to that
+	for key in list(common.keys()):
+		common[key] = float(sum(common[key]))/float(len(common[key]))
+
+	return common
+
 
 if __name__ == "__main__":
 	app = ClarifaiApp(CLARIFAI_APP_ID, CLARIFAI_APP_SECRET)
@@ -38,15 +57,22 @@ if __name__ == "__main__":
 			#need to remove the thumbnail letter imgur adds to its links to get HD
 			MODEL_LINKS.append(src[0:len(src)-5] + src[len(src)-4:len(src)])
 
-	#predict with clarifai api what the images are most commonly
-	#model = app.models.get("general-v1.3")
-	#output = model.predict_by_url(url='https://samples.clarifai.com/metro-north.jpg')
+	#predict with clarifai api what the images are most commonly representative of
+	model = app.models.get("general-v1.3")
+	predictions = []
 
-	#predictions = output["outputs"][0]["data"]["concepts"]
-	#for prediction in predictions:
-		#print(prediction["name"], '{0:.4g}'.format(prediction["value"]*100))
+	#puts predictions into 2d array
+	print("Getting data from clarifai... (this may take a minute)")
+	for i in range(len(MODEL_LINKS)):
+		url = 'https://%s' % MODEL_LINKS[i]
+		output = model.predict_by_url(url=url)
+		for prediction in output["outputs"][0]["data"]["concepts"]:
+			predictions.append([prediction["name"], prediction["value"]])
 
-	#print reccomendations
+	common = mostCommon(predictions)
 
-	print(MODEL_LINKS)
+	#get sorted tuple
+	commonTuple = sorted(common.items(), key=lambda x:x[1])
+	print(commonTuple)
+
 	print("Complete.")
